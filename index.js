8bcb4bf24d6acad91fb9766476c6e797614a131a
Conor Kelly <ckelly@ckellydesign.net>
2016-06-23 11:32:33 +0300

    adding email service for ona webhook test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var nodemailer = require('nodemailer');
 // var csv = require('csv-stream');
 var path = require('path');
 // var multer = require('multer');
@@ -24,6 +25,35 @@ app.get('/form/:formID', function(req, res){
 	request('https://api.ona.io/api/v1/data/' + formID, function (error, response, body){
 		res.send(response);
 	});
+});
+
+app.use('/formHook', function (req, res) {
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'conor.kidogo@gmail.com',
+			pass: 'welovebabies'
+		}
+	});
+
+	var text = res.body,
+			mailOptions = {
+				from: 'conor.kidogo@gmail.com',
+				to: 'ckellydesign.net@gmail.com',
+				subject: 'test email!!',
+				text: text
+	};
+
+	transporter.sendMail(mailOptions, function (err, info) {
+		if (err) {
+			console.log('sendMail Err -', err);
+			res.json({ yo: 'error' });
+		} else {
+			console.log('mail sent -', info.response);
+			res.json({ yo: info.response });
+		}
+	});
+
 });
 
 app.use(allowCrossDomain);
