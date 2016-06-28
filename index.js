1c87f2b91b7b78211be85bb48e34f10dcfa2d5f4
Conor Kelly <ckelly@ckellydesign.net>
2016-06-28 10:38:13 +0300

    fixing toTitleCase()

--- index.js
+++ index.js
@@ -130,8 +130,8 @@ function parseFormSubmission (data) {
 	return str;
 }
 
-String.prototype.toTitleCase = function (str) {
-	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+String.prototype.toTitleCase = function () {
+	return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
 app.listen(process.env.PORT || 9000, function(){
